16684ee8b5a7ef190f3ce2616708261914847ab7
Luiz Felipe <lfpereira323@gmail.com>
2023-08-09 21:05:51 -0300

    create project and git repository

---
+++ src/DbIntegrityException.java
@@ -0,0 +1,8 @@
+public class DbIntegrityException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public DbIntegrityException(String msg) {
+        super(msg);
+    }
+}
